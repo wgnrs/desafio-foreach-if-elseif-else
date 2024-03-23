572c313345e67118f8ff08239306f326e8550e85
Wagner <wagner_silvajr@hotmail.com>
2024-03-23 13:11:30 -0300

    feat: finalização de desafio

--- Program.cs
+++ Program.cs
@@ -1,2 +1,137 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+This C# console application is designed to:
+- Use arrays to store student names and assignment scores.
+- Use a `foreach` statement to iterate through the student names as an outer program loop.
+- Use an `if` statement within the outer loop to identify the current student name and access that student's assignment scores.
+- Use a `foreach` statement within the outer loop to iterate though the assignment scores array and sum the values.
+- Use an algorithm within the outer loop to calculate the average exam score for each student.
+- Use an `if-elseif-else` construct within the outer loop to evaluate the average exam score and assign a letter grade automatically.
+- Integrate extra credit scores when calculating the student's final score and letter grade as follows:
+    - detects extra credit assignments based on the number of elements in the student's scores array.
+    - divides the values of extra credit assignments by 10 before adding extra credit scores to the sum of exam scores.
+- use the following report format to report student grades: 
+
+    Student         Grade
+
+    Sophia:         92.2    A-
+    Andrew:         89.6    B+
+    Emma:           85.6    B
+    Logan:          91.2    A-
+*/
+int examAssignments = 5;
+
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
+
+int[] studentScores = new int[10];
+
+string currentStudentLetterGrade = "";
+
+// display the header row for scores/grades
+Console.Clear();
+Console.WriteLine("Student\t\tExam Score\tOverall Grade\tExtra Credit\n");
+
+/*
+The outer foreach loop is used to:
+- iterate through student names 
+- assign a student's grades to the studentScores array
+- sum assignment scores (inner foreach loop)
+- calculate numeric and letter grade
+- write the score report information
+*/
+foreach (string name in studentNames)
+{
+    string currentStudent = name;
+
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+
+    double sumAssignmentScores = 0;
+
+    double assignmentExtraScore = 0;
+
+    double currentStudentGrade = 0;
+
+    int gradedAssignments = 0;
+
+    double overallStudentGrade = 0;
+
+    foreach (double score in studentScores)
+    {
+        gradedAssignments += 1;
+
+        if (gradedAssignments <= examAssignments)
+        {
+            sumAssignmentScores += score;
+        }
+        else
+        {
+            assignmentExtraScore += score / 10;
+        } 
+    }
+
+    currentStudentGrade = sumAssignmentScores / examAssignments;
+
+    overallStudentGrade = (sumAssignmentScores + assignmentExtraScore) / examAssignments;
+
+    if (currentStudentGrade >= 97)
+        currentStudentLetterGrade = "A+";
+
+    else if (overallStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
+
+    else if (overallStudentGrade >= 90)
+        currentStudentLetterGrade = "A-";
+
+    else if (overallStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+
+    else if (overallStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
+
+    else if (overallStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
+
+    else if (overallStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
+
+    else if (overallStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
+
+    else if (overallStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
+
+    else if (overallStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+
+    else if (overallStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+
+    else if (overallStudentGrade >= 60)
+        currentStudentLetterGrade = "D-";
+
+    else
+        currentStudentLetterGrade = "F";
+
+    // Student         Grade
+    // Sophia:         92.2    A-
+
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t\t{overallStudentGrade:0.00}\t{currentStudentLetterGrade}\t{(int)currentStudentGrade} ({assignmentExtraScore/5:0.00} pts)");
+}
+
+// required for running in VS Code (keeps the Output windows open to view results)
+Console.WriteLine("\n\rPress the Enter key to continue");
+Console.ReadLine();
--- obj/Debug/net8.0/desafio-forearch-if-elseif-else.AssemblyInfo.cs
+++ obj/Debug/net8.0/desafio-forearch-if-elseif-else.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("desafio-forearch-if-elseif-else")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e346e70640a1d06c714b7ae913bc2fa7f5d660e")]
 [assembly: System.Reflection.AssemblyProductAttribute("desafio-forearch-if-elseif-else")]
 [assembly: System.Reflection.AssemblyTitleAttribute("desafio-forearch-if-elseif-else")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
